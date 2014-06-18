d176f7697e8d0dcc1e07239b6df0328e915ecd54
Stefan Valentin <khalilravanna@gmail.com>
2014-06-17 20:01:00 -0500

    Minimal mistakes rip

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,106 @@
+'use strict';
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
+  grunt.initConfig({
+    jshint: {
       options: {
-        includePaths: require('node-bourbon').with('bower_components/foundation/scss', 'scss')
+        jshintrc: '.jshintrc'
       },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!assets/js/plugins/*.js',
+        '!assets/js/scripts.min.js'
+      ]
+    },
+    recess: {
       dist: {
         options: {
-          outputStyle: 'compressed'
+          compile: true,
+          compress: true
         },
         files: {
-          'css/app.css': 'scss/app.scss'
-        }        
+          'assets/css/main.min.css': [
+            'assets/less/main.less'
+          ]
+        }
       }
     },
-
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
-
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+            'assets/js/plugins/*.js',
+            'assets/js/_*.js'
+          ]
+        }
       }
+    },
+    imagemin: {
+      dist: {
+        options: {
+          optimizationLevel: 7,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'images/'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: '{,*/}*.svg',
+          dest: 'images/'
+        }]
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'assets/less/*.less'
+        ],
+        tasks: ['recess']
+      },
+      js: {
+        files: [
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint','uglify']
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/main.min.css',
+        'assets/js/scripts.min.js'
+      ]
     }
   });
 
-  grunt.loadNpmTasks('grunt-sass');
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-recess');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
-}
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'recess',
+    'uglify',
+    'imagemin',
+    'svgmin'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
